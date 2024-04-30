a0086025699c305fac047eea5d0ff87588cafb3c
Enzo Lagoela <lagoela.dev@gmail.com>
2024-04-30 18:17:09 -0300

    add toast and currency select now persists to sqlite db

--- app/components/CurrencyComboBox.tsx
+++ app/components/CurrencyComboBox.tsx
@@ -19,10 +19,12 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Currencies, Currency } from "@/lib/currencies";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import SkeletonWrapper from "./SkeletonWrapper";
 import { UserSettings } from "@prisma/client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { UpdateUserCurrency } from "../wizard/_actions/userSettings";
+import { toast } from "sonner";
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false);
@@ -44,28 +46,56 @@ export function CurrencyComboBox() {
     if (userCurrency) {
       setSelectedOption(userCurrency);
     }
+  }, [userSettings.data]);
+
+  const mutation = useMutation({
+    mutationFn: UpdateUserCurrency,
+    onSuccess: (data: UserSettings) => {
+      toast.success("Currency updated successfully 🥳", {
+        id: "update-currency",
+      });
+
+      setSelectedOption(
+        Currencies.find((c) => c.value === data.currency) || null
+      );
+    },
+    onError: (e) => {
+      toast.error("Something went wrong.", { id: "update-currency" });
+    },
   });
 
-  console.log("@@@ USER SETTINGS", userSettings);
+  const selectOption = useCallback(
+    (currency: Currency | null) => {
+      if (!currency) {
+        toast.error("Please select a currency.");
+        return;
+      }
+
+      toast.loading("Updating currency...", { id: "update-currency" });
+      mutation.mutate(currency.value);
+    },
+    [mutation]
+  );
 
   if (isDesktop) {
     return (
       <SkeletonWrapper isLoading={userSettings.isFetching}>
         <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger asChild>
-            <Button variant="outline" className="w-full justify-start">
+            <Button
+              variant="outline"
+              className="w-full justify-start"
+              disabled={mutation.isPending}
+            >
               {selectedOption ? (
                 <>{selectedOption.label}</>
               ) : (
-                <>+ Set Currency</>
+                <>Set Currency</>
               )}
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-[200px] p-0" align="start">
-            <OptionList
-              setOpen={setOpen}
-              setSelectedOption={setSelectedOption}
-            />
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
           </PopoverContent>
         </Popover>
       </SkeletonWrapper>
@@ -76,16 +106,17 @@ export function CurrencyComboBox() {
     <SkeletonWrapper isLoading={userSettings.isFetching}>
       <Drawer open={open} onOpenChange={setOpen}>
         <DrawerTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
+          <Button
+            variant="outline"
+            className="w-full justify-start"
+            disabled={mutation.isPending}
+          >
             {selectedOption ? <>{selectedOption.label}</> : <>+ Set Currency</>}
           </Button>
         </DrawerTrigger>
         <DrawerContent>
           <div className="mt-4 border-t">
-            <OptionList
-              setOpen={setOpen}
-              setSelectedOption={setSelectedOption}
-            />
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
           </div>
         </DrawerContent>
       </Drawer>
@@ -106,7 +137,7 @@ function OptionList({
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup>
-          {Currencies.map((currency) => (
+          {Currencies.map((currency: Currency) => (
             <CommandItem
               key={currency.value}
               value={currency.value}
