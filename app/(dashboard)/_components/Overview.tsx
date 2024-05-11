eebdacaa71165831b85dd0922ef63b504a2d9f95
Enzo Lagoela <lagoela.dev@gmail.com>
2024-05-11 11:32:52 -0300

    adiciona componente da visão principal, input de data, e input de range de data

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { UserSettings } from "@prisma/client";
+import { startOfMonth } from "date-fns";
+import React, { useState } from "react";
+
+function Overview({ userSettings }: { userSettings: UserSettings }) {
+  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+
+  return <div>
+    {userSettings.currency}
+  </div>;
+}
+
+export default Overview;
